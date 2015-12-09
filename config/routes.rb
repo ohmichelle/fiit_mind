60d42b6e8aecacff1164c6ad1d1a7a5aed7da830
ohmichelle <oh.michelles@gmail.com>
2015-12-08 23:56:14 -0600

    edit navbar sign-in/sign-out

--- config/routes.rb
+++ config/routes.rb
@@ -57,7 +57,7 @@ Rails.application.routes.draw do
   # Routes for the User resource:
   # READ
   get "/users", :controller => "users", :action => "index"
-  #get "/users/:id", :controller => "users", :action => "show"
+  get "/users/:id", :controller => "users", :action => "show"
 
   #get "/my_meals", :controller => "users", :action => "my_meals"
 
