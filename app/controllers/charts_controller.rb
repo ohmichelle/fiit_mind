adec5d68f35531c44cbbfc95ab52b2b9e4d8dd94
ohmichelle <oh.michelles@gmail.com>
2015-12-11 15:21:58 -0600

    setup chart kick, failed attempt

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,5 +1,11 @@
 class ChartsController < ApplicationController
   def index
+    @daily_records = current_user.daily_records
+    @daily_record = DailyRecord.new
 
+    @meals = Meal.where(:daily_record_id => @daily_records.select(:id))
+    @meal = Meal.new
+
+    @date = Date.today
   end
 end
